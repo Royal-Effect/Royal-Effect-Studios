f371a52f793bc8ad29897d8e9ce0b56f4a6faa5b
Adeluola Ayomide David <102674779+theStrategizt@users.noreply.github.com>
2026-07-01 12:04:25 +0100

    ee

--- royal-effect/proxy.ts
+++ royal-effect/proxy.ts
@@ -2,10 +2,15 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL('/home', request.url))
+export function proxy(request: NextRequest) {
+  // Prevent infinite redirects and 404s by not redirecting everything.
+  // E.g., if you had a '/home', you could redirect to '/':
+  if (request.nextUrl.pathname === '/home') {
+    return NextResponse.redirect(new URL('/', request.url))
+  }
+  return NextResponse.next()
 }
  
 // Alternatively, you can use a default export:
-// export default function middleware(request: NextRequest) { ... }
+// export default function proxy(request: NextRequest) { ... }
  
