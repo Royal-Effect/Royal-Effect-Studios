335a024714d5a8d752a62a1a677de935eeff9213
Adeluola Ayomide David <102674779+theStrategizt@users.noreply.github.com>
2026-07-02 11:31:45 +0100

    tiny modification made

--- royal-effect/components/ui/direction-aware-hover.tsx
+++ royal-effect/components/ui/direction-aware-hover.tsx
@@ -3,7 +3,7 @@
 import { useRef, useState } from "react";
 
 import { AnimatePresence, motion } from "motion/react";
-import { cn } from "@/lib/utils";
+import { cn } from "@/libs/utils/utils";
 
 export const DirectionAwareHover = ({
   imageUrl,
